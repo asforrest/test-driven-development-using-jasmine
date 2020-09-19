8da3d8459f191bf223f3b067329cf0bb067596f6
Alexander Forrest <asforrest@icloud.com>
2020-09-19 12:00:33 +0000

    Refactoring - breaking our tests

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,17 @@
 describe("Calculator", function() {
     describe("Addition tests", function() {
-        it("should return 42", function(){
+        it("should return 42", function() {
             expect(addition(20, 22)).toBe(42);
-        })
-    })
-})
+        });
+        it("should return 26", function() {
+            expect(addition(7, 19)).toBe(26);
+        });
+
+        it("Should return an error if we don't supply two numbers", function() {
+           // expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+           spyOn(window, "alert");
+           addition("Hitchhikers", "Guide");
+           expect(window.alert).toHaveBeenCalledWith("Error!");
+        });
+    });
+});
